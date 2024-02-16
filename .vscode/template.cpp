c8b12c0babf338a7106cbceec184e64a88459304
Duong Nguyen <dnx04@proton.me>
2024-02-16 21:40:05 +0700

    419

---
+++ .vscode/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int x;
+  cin >> x;
+  cout << x;
+}
